9ebe3c1d13dc77b1b52ce381a01554d1a25702d7
Nitipoom007 <hornitipoom007@gmail.com>
2026-03-27 14:21:19 +0700

    test deploy11.2

--- server1.js
+++ server1.js
@@ -6,13 +6,16 @@ const mysql = require('mysql2/promise');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 // const port = 3001;
 const port = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: 'https://projectposserver-production.up.railway.app'
+  origin: [
+    'http://localhost:3000',
+    'https://projectposserver-production.up.railway.app'
+  ]
 }));
 
 const pool = mysql.createPool({
